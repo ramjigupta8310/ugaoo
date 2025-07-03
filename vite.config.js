84fd36c6e330a6cd3ce78299938f07ae30ff1a9a
ramjigupta8310 <ramjigupta8310@gmail.com>
2025-07-03 12:06:39 +0530

    updating vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ugaoo/', // This is your repository name
+  base: '/', // This is your repository name
   plugins: [react()],
 })
